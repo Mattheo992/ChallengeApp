e4f9473c9ba8209571ad6192fb24aa9a68cec9e2
Mattheo992 <stepniak.matt@gmail.com>
2023-08-19 12:20:43 +0200

    Zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string gender = "kobieta";
+int age = 33;
+
+if (gender == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (gender != "kobieta" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine(" Ktoś zupełnie inny");
+}
+
+//Mateusz
