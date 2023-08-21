1e0a1dcc745e33bddc7a95d5531c7569f70e3cf6
Mattheo992 <stepniak.matt@gmail.com>
2023-08-21 20:33:19 +0200

    Zadanie domowe nr 6

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,22 +1,39 @@
-﻿string name = "Ewa";
-string gender = "kobieta";
-int age = 33;
+﻿using ChallengeApp;
 
-if (gender == "kobieta" && age < 30)
+Employee employee1 = new Employee("Mateusz", "Kowalski", 26);
+Employee employee2 = new Employee("Monika", "Sęk" , 31);
+Employee employee3 = new Employee("Zuzia", "Graszka" , 42);
+
+employee1.AddScore(5);
+employee1.AddScore(2);
+employee1.AddScore(7);
+employee1.AddScore(3);
+employee1.AddScore(1);
+
+employee2.AddScore(2);
+employee2.AddScore(2);
+employee2.AddScore(5);
+employee2.AddScore(8);
+employee2.AddScore(1);
+
+employee3.AddScore(2);
+employee3.AddScore(5);
+employee3.AddScore(6);
+employee3.AddScore(2);
+employee3.AddScore(1);
+
+List<Employee> employees = new List<Employee>()
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else if (name == "Ewa" && age == 33)
+employee1, employee2, employee3
+};
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+ foreach (var employee in employees)
 {
-    Console.WriteLine("Ewa, lat 33");
-}
-else if (gender != "kobieta" && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else
-{
-    Console.WriteLine(" Ktoś zupełnie inny");
+if(employee.Result > maxResult)
+    {
+        employeeWithMaxResult = employee;
+    }
 }
 
-//Mateusz
+Console.WriteLine("Pracownikiem z największą liczbą punktów jest " + employeeWithMaxResult.Name + " " + employeeWithMaxResult.Surname + ", wiek: " + employeeWithMaxResult.Age + " z liczbą punktów " +employeeWithMaxResult.Result);
