872965a5bdf0a04038367e1292e728ce04fb6a47
Mattheo992 <stepniak.matt@gmail.com>
2023-09-05 09:18:58 +0200

    Zadanie domowe dzień 15

--- ChallengeApp/ChallengeApp/Supervisor.cs
+++ ChallengeApp/ChallengeApp/Supervisor.cs
@@ -1,18 +1,19 @@
-﻿namespace ChallengeApp
+﻿
+
+namespace ChallengeApp
 {
-    public class Employee : IEmployee
+    public class Supervisor : IEmployee
     {
-
         private List<float> grades = new List<float>();
-
-        public Employee() { }
-        public Employee(string name, string surname)
+        public Supervisor(string name, string surname)
         {
             this.Name = name;
             this.Surname = surname;
         }
+
         public string Name { get; private set; }
         public string Surname { get; private set; }
+
         public void AddGrade(float grade)
         {
 
@@ -25,17 +26,76 @@
                 throw new Exception("Invalid grade value");
             }
         }
+
         public void AddGrade(string grade)
         {
-            if (float.TryParse(grade, out float result))
+            switch (grade)
             {
-                this.AddGrade(result);
-            }
-            else
-            {
-                throw new Exception("String is not a float");
+                case "6":
+                    this.AddGrade(100);
+                    break;
+                case "-6":
+                case "6-":
+                    this.AddGrade(95);
+                    break;
+                case "+5":
+                case "5+":
+                    this.AddGrade(85);
+                    break;
+                case "5":
+                    this.AddGrade(80);
+                    break;
+                case "-5":
+                case "5-":
+                    this.AddGrade(75);
+                    break;
+                case "+4":
+                case "4+":
+                    this.AddGrade(65);
+                    break;
+                case "4":
+                    this.AddGrade(60);
+                    break;
+                case "-4":
+                case "4-":
+                    this.AddGrade(55);
+                    break;
+                case "+3":
+                case "3+":
+                    this.AddGrade(45);
+                    break;
+                case "3":
+                    this.AddGrade(40);
+                    break;
+                case "-3":
+                case "3-":
+                    this.AddGrade(35);
+                    break;
+                case "+2":
+                case "2+":
+                    this.AddGrade(25);
+                    break;
+                case "2":
+                    this.AddGrade(20);
+                    break;
+                case "-2":
+                case "2-":
+                    this.AddGrade(15);
+                    break;
+                case "1":
+                    this.AddGrade(0);
+                    break;
+                default:
+                    throw new Exception("Wrong grade!");
+
             }
         }
+        public void AddGrade(int grade)
+        {
+            float result = (float)grade;
+            this.AddGrade(result);
+        }
+
         public void AddGrade(long grade)
         {
             if (grade >= 0 && grade <= 100)
@@ -47,16 +107,13 @@
                 throw new Exception("Invalid grade value");
             }
         }
-        public void AddGrade(int grade)
-        {
-            float result = (float)grade;
-            this.AddGrade(result);
-        }
+
         public void AddGrade(double grade)
         {
             float result = (float)grade;
             this.AddGrade(result);
         }
+
         public void AddGrade(char grade)
         {
             switch (grade)
@@ -78,11 +135,8 @@
                     break;
                 default:
                     throw new Exception("Wrong Letter");
-
-
             }
         }
-
         public Statistics GetStatistics()
         {
             var statistics = new Statistics();
@@ -122,10 +176,5 @@
             return statistics;
 
         }
-
     }
 }
-
-
-
-
